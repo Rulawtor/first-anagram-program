cd6cbcb9216bf92a0de9f0390588844786b71577
Rulawtor <79028766+Rulawtor@users.noreply.github.com>
2023-01-12 20:41:51 -0500

    small change to make all user input work

---
+++ Anagram program/anagrams.py
@@ -0,0 +1,17 @@
+# Created by Rulawtor
+# Credit to dwyl for the list of words
+
+import json
+words = json.load(open("words_dictionary.json", "r"))
+ref_word = input().lower()
+not_same_word = ref_word # this is used to make sure the program doesn't just spit the same word back at you
+
+for testWords in words:
+    words_broken_down = testWords # this breaks down all the letters in a word so that the program can compare the arrays and see if they are anagrams
+    words_broken_down = sorted(words_broken_down)
+    ref_word = sorted(ref_word)
+    
+    if words_broken_down == ref_word and testWords != not_same_word: # if the array of the word being tested to be an anagram is the same as the users words array, and the word being tested is not the same as your original word, print the word being tested
+        print(testWords)
+    else: # else go back to the top and check the next word
+        continue
---
+++ test.py
@@ -0,0 +1,2 @@
+import os
+print("Working dir:", os.getcwd())
